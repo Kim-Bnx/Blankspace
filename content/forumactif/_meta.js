166861ed228339b7c9f0f53a95257ee4ce3980ad
Kimberley Bonix <bonixkimberley@gmail.com>
2026-03-30 20:18:42 +0200

    Content integration

--- content/forumactif/_meta.js
+++ content/forumactif/_meta.js
@@ -1,9 +1,20 @@
-export default {
-    "###": {
-        type: "separator",
-        title: "Guide", // Title is optional
-    },
-    index: {
-        title: "Markdown syntax guide",
-    },
+import { MessagesSquare } from "lucide-react";export default {
+  forumactif: {
+    type: "separator",
+    title: "Introduction",
+  },
+  index: (
+    <>
+      <MessagesSquare />
+      <span>Forumactif</span>
+    </>
+  ),
+  principes: {
+    type: "separator",
+    title: "Principes",
+  },
+  structure: { title: "Structure d'un forum" },
+  styles: { title: "Styles et personnalisation" },
+  scripts: { title: "Scripts" },
+  methodo: { title: "Méthodologie de personnalisation" },
 };
